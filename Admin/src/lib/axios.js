e833b3c7628bc02270f6504e1b2dacbfba0d2c5f
Karan-Singh-Bhandari-19 <karanbhandari037@gmail.com>
2025-01-04 00:01:13 +0530

    updated backend url

--- Admin/src/lib/axios.js
+++ Admin/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_URL,
+    baseURL: 'https://emart-wap7.onrender.com',
     withCredentials: true,
 });
